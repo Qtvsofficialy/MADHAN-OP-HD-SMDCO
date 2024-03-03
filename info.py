ba52de6e17e7bba7d8ec5193d03ccf964e52ff00
Qtvsofficialy <144274079+Qtvsofficialy@users.noreply.github.com>
2024-03-04 00:59:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,26 +13,26 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
-BOT_TOKEN = environ.get('BOT_TOKEN', "")
+API_ID = int(environ.get('API_ID', '27204651'))
+API_HASH = environ.get('API_HASH', 'f16a3248741e877e483b8fb883dacf09')
+BOT_TOKEN = environ.get('BOT_TOKEN', "6598334942:AAE5CK98L-PQVcSoFuNNQ4-lM_Uf0BAjjbo")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 1800))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', '')).split() #SAMPLE PIC
+PICS = (environ.get('PICS', 'https://graph.org/file/c607e2f75626c47449946.jpg https://graph.org/file/9b06d5dd3861f731272fb.jpg https://graph.org/file/a11a38a73e16bb804c3b7.jpg https://graph.org/file/e00f900ffa96eca694b46.jpg https://graph.org/file/bc7e4769fdebe4062b335.jpg https://graph.org/file/f4e8102f7cdc96a9f133b.jpg https://graph.org/file/d6658e838bfb821e7247f.jpg https://graph.org/file/1386506d9c9ed5568e898.jpg https://graph.org/file/d01a531687d2e684eb79b.jpg https://graph.org/file/78ea3afb4dc18cb0443a9.jpg https://graph.org/file/412114a824811c6897c60.jpg https://graph.org/file/ec32b716e26d9b475ec2e.jpg https://graph.org/file/06f78d966309aa76969d7.jpg https://graph.org/file/26f66a3a9efc9d9ebc7c5.jpg https://graph.org/file/d6658e838bfb821e7247f.jpg https://graph.org/file/1386506d9c9ed5568e898.jpg https://graph.org/file/d01a531687d2e684eb79b.jpg https://graph.org/file/78ea3afb4dc18cb0443a9.jpg https://graph.org/file/7c6ce56085d8262a1b5b6.jpg https://graph.org/file/412114a824811c6897c60.jpg https://graph.org/file/ec32b716e26d9b475ec2e.jpg https://graph.org/file/06f78d966309aa76969d7.jpg https://graph.org/file/26f66a3a9efc9d9ebc7c5.jpg')).split() #SAMPLE PIC
 NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/e0770294280f17df23911.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://graph.org/file/5f4126f0a68828857243f.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://graph.org/file/f6466cc7f17f54ab0ff3e.jpg")
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '').split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1044163529 885675538').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001520430666').split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '1044163529 885675538').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '').split()]
-auth_channel = environ.get('AUTH_CHANNEL', '')
+auth_channel = environ.get('AUTH_CHANNEL', '-1001530306088')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
@@ -43,25 +43,25 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://qtmve:qtmve@cluster0.41lxnpf.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', '')
-SHORTLINK_API = environ.get('SHORTLINK_API', '')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'tnshort.net')
+SHORTLINK_API = environ.get('SHORTLINK_API', '02c93c55c4567035a37ffc32731d8f0e1c530f98')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
 PORT = environ.get("PORT", "8080")
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/SAM_DUB_LEEZZa')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/SAM_DUB_LEZHa')
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/SMD_Dulinks/28')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+z48f1nqE1FMyYmNl')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Qtmve_linkzZz')
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/Qtmve_link/3')
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 MSG_ALRT = environ.get('MSG_ALRT', '⚓𝐒𝐌𝐃_BOTz🍁')
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001723140225'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001870756864'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '+oZ27l2vnexphZTVl')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
@@ -75,7 +75,7 @@ LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False")
 SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
 INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
-FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).split()]
+FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1001520430666')).split()]
 MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
